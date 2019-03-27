e626bb357de1ae5d3df4ef762cabff026b4ef521
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-03-28 00:10:02 +0500

    feat(ImageBackground): Added ImageBackground component

---
+++ src/components/ImageBackground.tsx
@@ -0,0 +1,11 @@
+import { ImageBackgroundProps } from 'react-native';
+import { getComponent } from '../getComponent';
+
+export { ImageBackgroundProps };
+export const ImageBackground = getComponent<ImageBackgroundProps>('ImageBackground');
+
+/**
+ * Default props for ImageBackground component
+ */
+export const ImageBackgroundDefaultProps = {
+};
