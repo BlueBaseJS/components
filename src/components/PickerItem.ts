ceac0f196fda1cc6a1672e3e6244242884541f47
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-03-31 09:37:39 +0500

    feat(Picker): Added new typings to Picker component

---
+++ src/components/PickerItem.ts
@@ -0,0 +1,11 @@
+import { PickerItemProps } from 'react-native';
+import { getComponent } from '../getComponent';
+
+export { PickerItemProps };
+export const PickerItem = getComponent<PickerItemProps>('PickerItem');
+
+/**
+ * Default props for PickerItem component
+ */
+export const PickerItemDefaultProps = {
+};
