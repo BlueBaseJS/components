be414b8bd04ac4abf5ceaee44de3f9052baff251
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-13 15:03:17 +0500

    feat(📋 Menu): Added Menu component

---
+++ src/components/MenuItem.ts
@@ -0,0 +1,36 @@
+import { StyleProp, ViewStyle } from 'react-native';
+import { DynamicIconProps } from './DynamicIcon';
+import { getComponent } from '../getComponent';
+
+export interface MenuItemProps {
+	/** Title text for the MenuItem. */
+	title: string;
+
+	/** Icon to display for the MenuItem. */
+	icon?: DynamicIconProps;
+
+	/**
+	 * Whether the 'item' is disabled. A disabled 'item' is greyed out and
+	 * onPress is not called on touch.
+	 */
+	disabled?: boolean;
+
+	/** Function to execute on press. */
+	onPress?: () => void;
+
+	style?: StyleProp<ViewStyle>;
+}
+
+export const MenuItemDefaultProps = {};
+
+/**
+ */
+// tslint:disable: jsdoc-format
+/**
+ * # 📋 MenuItem
+ *
+ * A component to show a single list item inside a Menu.
+ */
+export const MenuItem = getComponent<MenuItemProps>('MenuItem');
+
+MenuItem.defaultProps = MenuItemDefaultProps;
