2853f21f88cb895acbf507fd9a70563a63324a83
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-16 15:34:20 +0500

    feat(🖥 Screen Observers): Added OrientationObserver & ScreenSizeOverserver

---
+++ src/components/OrientationObserver.ts
@@ -0,0 +1,14 @@
+import { RenderPropChildren, getComponent } from '@bluebase/core';
+
+export interface OrientationObserverProps {
+	children: RenderPropChildren<OrientationObserverState>
+}
+
+export interface OrientationObserverState {
+	isLandscape: boolean,
+}
+
+/**
+ * # 🖥 Observes changes in Screen Orientation
+ */
+export const OrientationObserver = getComponent<OrientationObserverProps>('OrientationObserver');
