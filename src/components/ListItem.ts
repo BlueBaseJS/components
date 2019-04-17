eb7cf257d3ff01f0d83f92957e31eb111eccd20d
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-17 23:03:30 +0500

    feat(📚 ListItem): selected prop added
    BREAKING CHANGE: active prop renamed to selected

--- src/components/ListItem.ts
+++ src/components/ListItem.ts
@@ -1,13 +1,46 @@
 import { getComponent } from '@bluebase/core';
 
 export interface ListItemProps {
+
+	/**
+	 * Title text for the list item.
+	 */
 	title: React.ReactNode;
+
+	/**
+	 * Description text for the list item.
+	 */
 	description?: React.ReactNode;
+
+	/**
+	 * React element to display on the left side.
+	 */
 	left?: React.ReactNode;
+
+	/**
+	 * React element to display on the right side.
+	 */
 	right?: React.ReactNode;
+
+	/**
+	 * Function to execute on press.
+	 */
 	onPress?: () => void;
-	active?: boolean;
+
+	/**
+	 * Is the list item selected
+	 */
+	selected?: boolean;
+
+	/**
+	 * Is the list item disabled
+	 */
 	disabled?: boolean;
 }
 
+/**
+ * 📚 ListItem
+ *
+ * A component to show tiles inside a List.
+ */
 export const ListItem = getComponent<ListItemProps>('ListItem');
