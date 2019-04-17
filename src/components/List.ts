bd5660bc7e81120da39719e386a011856f12b435
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-17 23:38:49 +0500

    feat(📚 List): Added sub components as static props

--- src/components/List.ts
+++ src/components/List.ts
@@ -1,3 +1,7 @@
+import { ListAvatar } from './ListAvatar';
+import { ListIcon } from './ListIcon';
+import { ListItem } from './ListItem';
+import { ListSubheader } from './ListSubheader';
 import React from 'react';
 import { ViewProps } from 'react-native';
 import { getComponent } from '@bluebase/core';
@@ -6,9 +10,21 @@ export interface ListProps extends ViewProps {
 	children?: React.ReactNode;
 }
 
+type ListType = React.ComponentType<ListProps> & {
+	Avatar: typeof ListAvatar;
+	Icon: typeof ListIcon;
+	Item: typeof ListItem;
+	Subheader: typeof ListSubheader;
+};
+
 /**
  * # 📚 List
  *
  * A component used to group list items.
  */
-export const List = getComponent<ListProps>('List');
+export const List = getComponent<ListProps>('List') as ListType;
+
+List.Avatar = ListAvatar;
+List.Icon = ListIcon;
+List.Item = ListItem;
+List.Subheader = ListSubheader;
