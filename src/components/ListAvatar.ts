e0b5aaff7e22a62a2154c8a4d5c2441241c589a0
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-17 23:24:09 +0500

    feat(📚 ListAvatar): ListAvatarProps now extends AvatarProps

--- src/components/ListAvatar.ts
+++ src/components/ListAvatar.ts
@@ -1,5 +1,11 @@
+import { AvatarProps } from './Avatar';
 import { getComponent } from '@bluebase/core';
 
-export interface ListAvatarProps {}
+export interface ListAvatarProps extends AvatarProps {}
 
+/**
+ * 📚 ListAvatar
+ *
+ * A component to show tiles inside a List.
+ */
 export const ListAvatar = getComponent<ListAvatarProps>('ListAvatar');
--- src/components/ListIcon.ts
+++ src/components/ListIcon.ts
@@ -1,17 +1,17 @@
 import { IconProps } from './Icon';
 import { getComponent } from '@bluebase/core';
 
-/**
- * 📚 ListIcon
- *
- * A component to show tiles inside a List.
- */
 export interface ListIconProps extends IconProps {}
 
 export const ListIconDefaultProps: Partial<ListIconProps> = {
 	size: 24
 };
 
+/**
+ * 📚 ListIcon
+ *
+ * A component to show tiles inside a List.
+ */
 export const ListIcon = getComponent<ListIconProps>('ListIcon');
 
 ListIcon.defaultProps = ListIconDefaultProps;
