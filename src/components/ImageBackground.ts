a197cb6fe05cd99c47107fd4a7a7584a7cf0a7ab
Abubakar Saddique <abubakar.mani1@gmail.com>
2019-05-02 19:18:47 +0500

    fix(ImageBackground): Imagebackground props cusomization

--- src/components/ImageBackground.ts
+++ src/components/ImageBackground.ts
@@ -1,10 +1,18 @@
-import { ImageBackgroundProps } from 'react-native';
+import { ImageBackgroundProps as BaseProps } from 'react-native';
 import { getComponent } from '@bluebase/core';
 
-export { ImageBackgroundProps };
+
 export const ImageBackground = getComponent<ImageBackgroundProps>('ImageBackground');
 
 /**
  * Default props for ImageBackground component
  */
 export const ImageBackgroundDefaultProps = {};
+
+export interface ImageBackgroundProps extends BaseProps {
+	/** Size of the icon. */
+	style:{
+		width:number,
+		height:number
+	}
+}
