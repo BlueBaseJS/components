169202e53e822eb580a5c5f0c33893ac5cbf0c0d
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-05-03 20:22:05 +0500

    feat(🖼 ImageBackground): Added support for BlueBase Assets

--- src/components/ImageBackground.ts
+++ src/components/ImageBackground.ts
@@ -1,7 +1,23 @@
-import { ImageBackgroundProps } from 'react-native';
+import { ImageBackgroundProps as RNImageBackgroundProps, ImageProps } from 'react-native';
 import { getComponent } from '@bluebase/core';
 
-export { ImageBackgroundProps };
+export interface ImageBackgroundProps extends RNImageBackgroundProps {
+	source: ImageProps['source'];
+}
+
+// tslint:disable: jsdoc-format
+/**
+ * # 🖼 ImageBackground
+ *
+ * A drop in replacement for React Native's ImageBackground component.
+ * Adds ability to display image stored in BlueBase Asset Registry.
+ *
+ * ## Usage
+ * ```jsx
+<ImageBackground resolve="Logo"><Content /></ImageBackground>
+<ImageBackground resolve={['LogoSquare', 'Logo']}><Content /></ImageBackground>
+```
+ */
 export const ImageBackground = getComponent<ImageBackgroundProps>('ImageBackground');
 
 /**
