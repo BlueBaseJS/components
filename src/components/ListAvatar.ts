ce1baa658ed4aee8864a87fb41d24d4f46a005a4
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-11-28 23:54:02 +0500

    feat(📚 ListAvatar): Added default size

--- src/components/ListAvatar.ts
+++ src/components/ListAvatar.ts
@@ -3,9 +3,14 @@ import { getComponent } from '@bluebase/core';
 
 export interface ListAvatarProps extends AvatarProps {}
 
+export const ListAvatarDefaultProps: Partial<ListAvatarProps> = {
+	size: 40,
+};
+
 /**
  * 📚 ListAvatar
  *
  * A component to show tiles inside a List.
  */
 export const ListAvatar = getComponent<ListAvatarProps>('ListAvatar');
+ListAvatar.defaultProps = ListAvatarDefaultProps;
