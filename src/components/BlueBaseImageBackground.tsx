324edfbb33cae31b4e96492ae1e46ddb85d36d53
Hashir Ch <info@iammhc.com>
2019-12-20 12:07:52 +0500

    feat(BlueBaseImageBackground): Added component (#116)

---
+++ src/components/BlueBaseImageBackground.tsx
@@ -0,0 +1,31 @@
+import {
+	ImageBackgroundProps as NImageBackgroundProps,
+	ImageProps as NImageProps,
+} from 'react-native';
+import { Omit, getComponent } from '@bluebase/core';
+export interface BlueBaseImageBackgroundProps extends Omit<NImageBackgroundProps, 'source'> {
+	/** Image source or BlueBase registry key */
+	source?: NImageProps['source'] | string | string[];
+}
+
+// tslint:disable: jsdoc-format
+/**
+ * # 🖼 BlueBaseImageBackground
+ *
+ * A drop in replacement for React Native's ImageBackground component.
+ * Adds ability to display image stored in BlueBase Asset Registry.
+ *
+ * ## Usage
+ * ```jsx
+<BlueBaseImageBackground resolve="Logo"><Content /></BlueBaseImageBackground>
+<BlueBaseImageBackground resolve={['LogoSquare', 'Logo']}><Content /></BlueBaseImageBackground>
+```
+ */
+export const BlueBaseImageBackground = getComponent<BlueBaseImageBackgroundProps>(
+	'BlueBaseImageBackground'
+);
+
+/**
+ * Default props for ImageBackground component
+ */
+export const BlueBaseImageBackgroundDefaultProps = {};
