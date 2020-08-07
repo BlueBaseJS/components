0e54758ee0bd7250ef35a11c08f7f86629f09b9f
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-08-07 14:35:10 +0500

    feat(🏓 TablePagination): Improved API

--- src/components/TablePagination.ts
+++ src/components/TablePagination.ts
@@ -1,36 +1,44 @@
-import { StyleProp, ViewStyle } from 'react-native';
+import { ViewStyle } from 'react-native';
 import { getComponent } from '@bluebase/core';
 
 export interface TablePaginationProps {
+	/**
+	 * The total number of rows.
+	 */
+	count: number;
+
 	/**
 	 * The currently visible page (starting with 0).
 	 */
 	page: number;
 
 	/**
-	 * The total number of pages.
-	 */
-	numberOfPages: number;
-
-	/**
-	 * Label text to display
-	 */
-	label?: React.ReactNode;
-
-	/**
 	 * Function to execute on page change.
 	 */
 	onPageChange: (page: number) => void;
-	/**
-	 * The total number of rows.
-	 */
 
-	count: number;
-	children: React.ReactNode;
-	style?: StyleProp<ViewStyle>;
+	/**
+	 * The number of rows per page.
+	 */
+	rowsPerPage: number;
+
+	/**
+	 * Customizes the options of the rows per page select field.
+	 * If less than two options are available, no select field will be displayed.
+	 */
+	rowsPerPageOptions?: number[];
+
+	/**
+	 * Callback fired when the number of rows per page is changed.
+	 */
+	onChangeRowsPerPage?: (rowsPerPage: number) => void;
+
+	style?: ViewStyle;
 }
 
-export const TablePaginationDefaultProps = {};
+export const TablePaginationDefaultProps: Partial<TablePaginationProps> = {
+	rowsPerPageOptions: [10, 25, 50, 100],
+};
 
 // tslint:disable: jsdoc-format
 /**
